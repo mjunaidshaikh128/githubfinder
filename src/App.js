f3ad163f24d5e2814a873741ca196b01e05f89d5
mjunaidshaikh128 <115004219+mjunaidshaikh128@users.noreply.github.com>
2023-07-15 13:30:58 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/layouts/Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+    </div>
+  )
+}
