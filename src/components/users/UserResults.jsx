5843b9f761aaae7dacebf10df689cd1fbb7a5c8d
mjunaidshaikh128 <115004219+mjunaidshaikh128@users.noreply.github.com>
2023-07-16 21:14:34 +0500

    context and reducer added/fetchusers

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,26 +1,22 @@
 import { useEffect, useState } from "react"
 import Spinner from "../layouts/Spinner"
+import UserItem from "./UserItem"
+import { useContext } from "react"
+import GithubContext from "../../context/github/GithubContext"
 
 const UserResults = () => {
-    const [users, setUsers] = useState([])
-    const [isLoading, setIsloading] = useState(true)
+    const { users, isLoading, fetchUsers } = useContext(GithubContext)
 
     useEffect(() => {
         fetchUsers()
 
     }, [])
 
-    const fetchUsers = async () => {
-        const response = await fetch("https://api.github.com/users")
-        const data = await response.json()
-        setUsers(data)
-        setIsloading(false)
-    }
 
     if (!isLoading) {
         return (
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-                {users.map((user) => <h3>{user.login}</h3>)}
+                {users.map((user) => <UserItem key={user.id} user={user} />)}
             </div>)
     } else return <Spinner />
 }
